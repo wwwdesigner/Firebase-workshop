b046d8cee9f54fb4de3b295ba9269412f662f37b
wwwdesigner <jakub.matwiejczyk@gmail.com>
2017-10-13 10:38:55 +0200

    user list ORDERBYCHILD

--- app/src/App.js
+++ app/src/App.js
@@ -6,16 +6,12 @@ class App extends Component {
 
 
     componentWillMount() {
-        for (let i = 0; i < 100; i++) {
-            fetch('http://cors-proxy.htmldriven.com/?url=https://randomuser.me/api')
-                .then((response) => {
-                    return response.json()
-                })
-                .then((data) => {
-                    let userData = JSON.parse(data.body).results[0]
-                    database.ref('userList').push(userData)
-                })
-        }
+                    database.ref('userList').orderByChild('name/first').once('value')
+                        .then((snapshot)=> {
+                            snapshot.forEach((childSnapshot) => {
+                                console.log(childSnapshot.val().name.first)
+                            })
+                        })
     }
 
     render() {
